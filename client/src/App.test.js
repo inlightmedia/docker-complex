e50026f0f6b9272e02a68ccfed8996adea396035
Joshua Dyck <josh@cyno.ca>
2018-10-08 16:23:40 -0400

    initial commit

---
+++ client/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+  // This test will fail in production bc there is no server in the test environment - to get it to work we would have to spit back some fake JSON
+});
